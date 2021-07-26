6230ad6f8236abd2b0585feaab34abcc237ec668
Andres Benitez <abolnaits@hotmail.com>
2021-07-26 13:14:30 -0500

    App
    Render Element

---
+++ src/components/RenderElement.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class RenderElement{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        
+        return(
+            <div>
+                <h1>Render Element</h1>
+            </div>
+        )
+    }
+}
