684d3bde5ef165285fd48d66a0af7be4135279cc
Andres Benitez <abolnaits@hotmail.com>
2021-08-02 12:43:03 -0500

    Update ComponentLifeCycle.js

--- src/components/ComponentLifeCycle.js
+++ src/components/ComponentLifeCycle.js
@@ -1,5 +1,25 @@
 import React,{Component, Fragment} from 'react';
 
+class ComponentClock extends Component{
+    constructor(props){
+        super(props);
+
+    }
+
+    componentWillUnmount(){
+        console.log('mount cycle 3:','componentWillUnmount');
+        
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <p>Time: {this.props.my_time}</p>
+            </Fragment>
+        )
+    }
+}
+
 class ComponentLifeCycle extends Component{
 
     constructor(props){
@@ -9,7 +29,7 @@ class ComponentLifeCycle extends Component{
         //state
         this.state = {
             my_date : new Date().toLocaleTimeString(),
-        
+            visible: false
         }
 
         this.timer = null;
@@ -19,10 +39,16 @@ class ComponentLifeCycle extends Component{
         console.log('mount cycle 1:','componentDidMount');
     }
 
+    
     componentDidUpdate(prevProps,prevState){
         console.log('mount cycle 2:','componentDidUpdate');
         console.log(prevProps);
         console.log(prevState);
+        
+    }
+
+    componentWillUnmount(){
+        console.log('mount cycle 3:','componentWillUnmount');
         
     }
 
@@ -34,6 +60,7 @@ class ComponentLifeCycle extends Component{
             //Update state
             this.setState({
                 my_date: new Date().toLocaleTimeString()
+                
             });
         },1000);
         
@@ -43,20 +70,25 @@ class ComponentLifeCycle extends Component{
     }
     start_clock = (e) =>{
         this.tic_tac();
+        this.setState({
+            visible:true
+        });
     }
 
     stop_clock = (e) =>{
         console.log('timer',this.timer);
         clearInterval(this.timer);
+        this.setState({
+            visible:false
+        });
     }
     render(){
         console.log('mount cycle 4: ','render');
         return(
             <Fragment>
                 <h1>Component Life Cycle</h1>
-                <p>
-                    {this.state.my_date}
-                </p>
+                {this.state.visible ? <ComponentClock my_time={this.state.my_date}></ComponentClock> : null}
+                
                 <button onClick={this.start_clock}>Start</button>
                 <button onClick={this.stop_clock}>Stop</button>
                 
